05915b2fb7dde400da6935d43eba34ecd228ded0
2022bcs0052-hubertmathew <hubertmathew2004@gmail.com>
2026-01-23 21:50:59 +0530

    Change predict endpoint to accept JSON input instead of form-data

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import numpy as np
+from typing import List
 
 app = FastAPI(title="ML Inference API")
 
 # Load trained model
 model = joblib.load("model.pkl")
 
+# Request schema
+class Features(BaseModel):
+    features: List[float]
+
 @app.get("/")
 def home():
     return {"message": "Model API is running"}
 
 @app.post("/predict")
-def predict(features: str = Form(...)):
-    """
-    features should be comma-separated numbers:
-    Example: "13.2,2.77,2.51,18.5,..."
-    """
-
-    # Convert string → list of floats
-    try:
-        values = [float(x) for x in features.split(",")]
-    except:
-        return {"error": "Invalid input format"}
-
-    data = np.array(values).reshape(1, -1)
-    prediction = model.predict(data)[0]
+def predict(data: Features):
+    values = data.features
+    arr = np.array(values).reshape(1, -1)
+    prediction = model.predict(arr)[0]
 
     return {
         "name": "Hubert",
